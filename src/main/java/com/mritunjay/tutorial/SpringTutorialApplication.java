3e4d1337a102582580af5dd912a1b3b8eba17fb4
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-03-10 13:26:38 +0530

    Updated SpringTutorialApplication and application.properties

--- src/main/java/com/mritunjay/tutorial/SpringTutorialApplication.java
+++ src/main/java/com/mritunjay/tutorial/SpringTutorialApplication.java
@@ -1,13 +1,22 @@
 package com.mritunjay.tutorial;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringTutorialApplication {
+@SpringBootApplication // Marks this as a Spring Boot application.
+public class SpringTutorialApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringTutorialApplication.class, args);
-	}
+    @Autowired // Spring will automatically inject the appropriate DB implementation.
+    private DB db;
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorialApplication.class, args); // Starts the Spring Boot application.
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(db.getData()); // Calls getData() method from the injected DB instance.
+    }
 }
