022ec1c200fbed9067fb768b01c50f7abb0768c5
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-03-11 17:05:24 +0530

    Changed isActive from boolean to Boolean in EmployeeDTO for PATCH support

--- src/main/java/com/mritunjay/tutorial/dto/EmployeeDTO.java
+++ src/main/java/com/mritunjay/tutorial/dto/EmployeeDTO.java
@@ -1,4 +1,23 @@
 package com.mritunjay.tutorial.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data // Lombok annotation to generate getters, setters, equals, hashCode, and toString
+@AllArgsConstructor // Lombok annotation to generate a constructor with all fields
+@NoArgsConstructor // Lombok annotation to generate a no-argument constructor
 public class EmployeeDTO {
+
+    private Long id; // Employee ID
+
+    private String name; // Employee name
+
+    private LocalDate dateOfJoining; // Employee's date of joining
+
+    @JsonProperty("isActive") // Ensures JSON uses "isActive" instead of "active"
+    private Boolean isActive;
 }
