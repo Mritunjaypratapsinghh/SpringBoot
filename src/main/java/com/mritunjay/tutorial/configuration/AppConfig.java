a3b2dee2154014b2c9c78678538871f5b612bca8
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-03-11 17:05:24 +0530

    Updated AppConfig for better bean management

--- src/main/java/com/mritunjay/tutorial/configuration/AppConfig.java
+++ src/main/java/com/mritunjay/tutorial/configuration/AppConfig.java
@@ -3,22 +3,37 @@ package com.mritunjay.tutorial.configuration;
 import com.mritunjay.tutorial.DB;
 import com.mritunjay.tutorial.DevDB;
 import com.mritunjay.tutorial.ProdDB;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration // Marks this class as a configuration class for Spring.
+@Configuration // Marks this class as a Spring configuration class
 public class AppConfig {
 
+    /**
+     * Creates a DevDB bean if the property `project.mode=development` is set in `application.properties`
+     */
     @Bean
     @ConditionalOnProperty(name = "project.mode", havingValue = "development")
     public DB getDevDBBean() {
-        return new DevDB(); // Creates a DevDB bean if project.mode=development
+        return new DevDB();
     }
 
+    /**
+     * Creates a ProdDB bean if the property `project.mode=production` is set in `application.properties`
+     */
     @Bean
     @ConditionalOnProperty(name = "project.mode", havingValue = "production")
     public DB getProdDBBean() {
-        return new ProdDB(); // Creates a ProdDB bean if project.mode=production
+        return new ProdDB();
+    }
+
+    /**
+     * Creates a ModelMapper bean for object mapping
+     */
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
     }
 }
