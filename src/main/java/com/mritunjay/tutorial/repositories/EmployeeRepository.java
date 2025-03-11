de7546162306635020f843e43293b9291501aa5e
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-03-11 17:05:24 +0530

    No major changes, just formatting improvements

--- src/main/java/com/mritunjay/tutorial/repositories/EmployeeRepository.java
+++ src/main/java/com/mritunjay/tutorial/repositories/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package com.mritunjay.tutorial.repositories;
 
-public class EmployeeRepository {
+import com.mritunjay.tutorial.entities.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Marks this interface as a Spring-managed repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 }
